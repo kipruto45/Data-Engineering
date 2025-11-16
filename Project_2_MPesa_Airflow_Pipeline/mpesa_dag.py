d7c9ad3eeac0c1d8c68b9a9bcb5ea0c84ad0d13c
Your Name <your.email@gmail.com>
2025-11-16 05:07:04 +0300

    Fix Airflow TaskGroup import: Use airflow.models instead of airflow.utils.task_group for compatibility

--- Project_2_MPesa_Airflow_Pipeline/mpesa_dag.py
+++ Project_2_MPesa_Airflow_Pipeline/mpesa_dag.py
@@ -9,7 +9,7 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
-from airflow.utils.task_group import TaskGroup
+from airflow.models import TaskGroup
 import logging
 
 logger = logging.getLogger(__name__)
